b4b9be29a2fada903bae05e8239286f01a357a35
deepbluezen <deepbluezen@users.noreply.github.com>
2026-04-11 19:28:39 +0100

    RSUV shader pick colour from pallete if vertex colour blue, offset by RSUV value

--- Assets/visual scripts/Helper scripts/SetRSUV.cs
+++ Assets/visual scripts/Helper scripts/SetRSUV.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-/* [ExecuteInEditMode]
-[ExecuteAlways] */
+[ExecuteInEditMode]
+[ExecuteAlways]
 
 public class SetRSUV : MonoBehaviour
 {
@@ -24,9 +24,17 @@ public class SetRSUV : MonoBehaviour
           
     }
 
-    // Update is called once per frame
-    void Update()
+     void OnValidate()
     {
-        
+          MeshRenderer[] allMeshRenderers = FindObjectsByType<MeshRenderer>(FindObjectsSortMode.InstanceID);
+        foreach (MeshRenderer meshRenderer in allMeshRenderers)
+        {
+            // compute a random color
+            Color32 c = Color.HSVToRGB(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0.6f, 1.0f), UnityEngine.Random.Range(0.6f, 1.0f));
+
+            // set it as a LDR color in the RSUV value of the renderer
+            uint cc = ((uint)c.b << 16) | ((uint)c.g << 8) | ((uint)c.r << 0);
+            meshRenderer.SetShaderUserValue(cc);
+        }
     }
 }
