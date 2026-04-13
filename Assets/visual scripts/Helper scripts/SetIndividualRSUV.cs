62649f7729004ccb1d63e0410dd2fc516bd95c80
deepbluezen <deepbluezen@users.noreply.github.com>
2026-04-13 11:49:23 +0100

    Individual set RSUV script so ready for linking those values into shader

--- Assets/visual scripts/Helper scripts/SetIndividualRSUV.cs
+++ Assets/visual scripts/Helper scripts/SetIndividualRSUV.cs
@@ -25,20 +25,29 @@ public class SetIndividualRSUV : MonoBehaviour
         none, some,somemore, all 
     } 
     
+    public enum MeshRotation
+    {
+        None,Rotate
+    }
 
+    public enum Emission
+    {
+        None,Shiny
+    }
 
     public HighlightPallete highlightPallete = HighlightPallete.Orange;
     public MainPallete mainPallete = MainPallete.Grey;
     public HideMeshPart hideMeshPart  = HideMeshPart.none;
-    public bool MeshRotation = false;
+    public MeshRotation meshRotation = MeshRotation.None;
     [Range(0,15)]
     public int MeshRotationMagnitude = 0;
     public MeshOffsetAxis meshoffsetAxis = MeshOffsetAxis.x;
     [Range(0,31)]
     public int MeshOffsetMagnitude = 0;
-    public bool Emission = false;
+    public Emission emission = Emission.None;
     [Range(0,7)]
     public int Smoothness = 0;
+    private int Random = 0;
 
     void Start()
     {
@@ -52,13 +61,26 @@ public class SetIndividualRSUV : MonoBehaviour
 
 void UpdateData()
     {
+        this.Random = UnityEngine.Random.Range(0,255);
         uint data = 0x00000000; // All bits set to 0
-        
+                
         data = HelpersRSUV.EncodeData(data, (int)highlightPallete, 0, 3);
-        
+        data = HelpersRSUV.EncodeData(data, (int)mainPallete, 3, 3);
+        data = HelpersRSUV.EncodeData(data, (int)hideMeshPart, 6, 2);
+        data = HelpersRSUV.EncodeData(data, (int)meshRotation, 8, 1);
+        data = HelpersRSUV.EncodeData(data, (int)MeshRotationMagnitude, 9, 4);
+        data = HelpersRSUV.EncodeData(data, (int)meshoffsetAxis, 13, 2);
+        data = HelpersRSUV.EncodeData(data, (int)MeshOffsetMagnitude, 15, 5);
+        data = HelpersRSUV.EncodeData(data, (int)emission, 20, 1);
+        data = HelpersRSUV.EncodeData(data, (int)Smoothness, 21, 3);
+        data = HelpersRSUV.EncodeData(data, (int)Random, 24, 8);
+
+
 
         //only seem to be able to set RSUV not get from meshrenderer in c#
-        //so log out value are setting 
+        //so log out value are setting, don't seem to be able to output in binary format
+        //so would have to convert back from that to see bits in online calculator
+        //hex is option but that isn't any easier to read
         Debug.Log(data);
     }
 
